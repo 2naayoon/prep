ba9fb9b607b816420edb48b318c8a681a2052c4c
2naaayoon <inayoon78@gmail.com>
2024-01-30 18:01:53 +0900

    제약조건

--- system.sql
+++ system.sql
@@ -9,3 +9,41 @@ ALTER USER SCOTT QUOTA 10M ON users;
 
 -- 
 alter session set "_oracle_script"=true;
+
+-- 계정 잠금 해제
+ALTER USER scott account unlock;
+
+-- DBA 권한을 가진 사용자만 사용자를 생성하고 권한 부여
+-- SYSTEM, sys as sysdba
+CREATE USER 아이디 IDENTIFIED BY 비밀번호;
+
+-- test 생성
+-- *Error : 공통 사용자 또는 롤 이름이 부적합합니다
+-- 21c XE 요구사항
+CREATE USER c##test IDENTIFIED BY test;
+
+-- CREATE SESSION 권한을 가지고있지 않음 - 로그온이 거절
+-- 권한 부여
+-- SESSION, CREATE (USER, TABLE, SEQUENCE, VIEW, ...)
+-- 권한 그룹 → 롤
+-- CONNECT, RESOURCE
+-- CONNECT (SESSION, CREATE TABLE, CREATE VIEW, ...)
+-- RESOURCE (SEQUENCE, 프로시저, 테이블, ...)
+
+GRANT 권한이름, ...... TO 아이디
+GRANT 롤이름, ...... TO 아이디
+
+GRANT CONNECT, resource TO c##test;
+
+CREATE USER c##test2 IDENTIFIED BY test
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+quota 10M ON users;
+
+GRANT CONNECT, resource TO c##test2;
+
+-- REVOKE 취소할 권한이름, ... FROM 아이디
+
+-- 비밀번호 변경
+ALTER USER c##test2 IDENTIFIED BY test;
+
