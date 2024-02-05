de62e250c4faac20f577dd5b425ad64bda202c31
2naaayoon <inayoon78@gmail.com>
2024-02-05 17:52:36 +0900

    배열

--- ch3_array/src/array/ArrayEx10.java
+++ ch3_array/src/array/ArrayEx10.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 /*  알고리즘 - 정렬
  *            선택정렬, 버블정렬, 힙정렬
- *
- *
  */
 
 public class ArrayEx10 {
@@ -22,6 +20,26 @@ public class ArrayEx10 {
         arr[min] = temp;
       }
     }
+    System.out.println(Arrays.toString(arr));
+
+    int i = 0, j = 0;
+    for (i = 0; i < arr.length; i++) {
+      int min_idx = i;
+
+      //최솟값을 갖고있는 인덱스 찾기
+      for (j = 1; j < arr.length; j++) {
+        if (arr[j] < arr[min_idx]) {
+          min_idx = j;
+        }
+      }
+      int temp = arr[i];
+      arr[i] = arr[min_idx];
+      arr[min_idx] = temp;
+    }
+    System.out.println(Arrays.toString(arr));
+
+    // 자바가 제공하는 sort (오름차순)
+    Arrays.sort(arr);
     System.out.println(Arrays.toString(arr));
   }
 }
