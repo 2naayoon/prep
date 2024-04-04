86d2dded625b9fb14e8cbdb82892581acc974f12
2naaayoon <inayoon78@gmail.com>
2024-04-04 17:56:29 +0900

    프로젝트 구조 n:1 관계

--- jpa/src/main/java/com/example/jpa/repository/MemoRepository.java
+++ jpa/src/main/java/com/example/jpa/repository/MemoRepository.java
@@ -2,9 +2,12 @@ package com.example.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.jpa.dto.MemoDto;
 import com.example.jpa.entity.Memo;
 
 // <Entity, Entity에서 pk로 쓰이는 컬럼 타입> : ~와 연결 되어 있음
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    void save(MemoDto dtoToEntity);
     // DAO 역할
 }
