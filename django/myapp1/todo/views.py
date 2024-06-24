2bc7f0fe62ae0230141cc2bce7e7b5dc53ffb4b0
2naayoon <inayoon78@gmail.com>
2024-06-24 18:13:31 +0900

    장고 기초

---
+++ django/myapp1/todo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# def list(request):
+#     """
+#     일반 문자열 응답
+#     """
+#     return HttpResponse("Hello")
+
+def list(request):
+    """
+    html 응답
+    """
+    return render(request, "todo/todo_list.html")
