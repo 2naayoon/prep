d4d3c9294070d9151e81d19d805da05889ef4271
2naayoon <inayoon78@gmail.com>
2024-07-01 18:06:45 +0900

    board, blog

---
+++ django/blog/blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        # title, content, user, image, created_at, modified_at 전부 포함
+        # user 막아놨을 때 오류 발생
+        # fields = '__all__'
+        # user 제외하고 나열 (~at 은 자동생성이라 제외)
+        fields = ["title","content","image"]
